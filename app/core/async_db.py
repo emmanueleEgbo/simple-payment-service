86f842214f791ff1668a7fc56936ab11ab29af7e
etechpro <technov8ive@gmail.com>
2026-05-16 22:49:13 +0100

    feat: setting up SQLAlchemy db config

--- app/core/async_db.py
+++ app/core/async_db.py
@@ -2,3 +2,8 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
+
+# Load environment variable from `.env` file
+load_dotenv()
+
+# Connection string for the async database
