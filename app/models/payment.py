2a1e9ecbb6e4f7b153ede31dfc145777149e755b
etechpro <technov8ive@gmail.com>
2026-05-17 23:57:05 +0100

    feat: add payment model

--- app/models/payment.py
+++ app/models/payment.py
@@ -1,1 +1,8 @@
 """Defines the SQLAlchemy ORM model representing a Payment entity."""
+
+from sqlalchemy import Column, Integer, String
+from app.core.async_db import Base
+
+
+class Payment(Base):
+    pass
