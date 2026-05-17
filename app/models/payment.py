a3ebf8c6f03214926db58a0ac2547f2b07cc10e8
etechpro <technov8ive@gmail.com>
2026-05-18 00:07:36 +0100

    feat: add payment model

--- app/models/payment.py
+++ app/models/payment.py
@@ -1,8 +1,21 @@
 """Defines the SQLAlchemy ORM model representing a Payment entity."""
 
-from sqlalchemy import Column, Integer, String
+import uuid
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, Enum
+from sqlalchemy.dialects.postgresql import UUID
 from app.core.async_db import Base
 
 
+# Payment status enum
+
+
 class Payment(Base):
-    pass
+    __tablename__="payments"
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
