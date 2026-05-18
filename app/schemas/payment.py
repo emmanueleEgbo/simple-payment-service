08e1f06f559dae624ea16d4ffc4176d83edd69a0
etechpro <technov8ive@gmail.com>
2026-05-18 15:39:11 +0100

    feat: add schema for create payment request

---
+++ app/schemas/payment.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+
+
+class CreatePaymentRequest(BaseModel):
+    user_id: str
+    amount: int = Field(gt=0)
+    currency: str
+    provider: str
+    reference: str | None   = None
+    description: str | None = None
