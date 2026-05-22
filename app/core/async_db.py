01b12c312f643afbd7f1903f3cb21a309dd9d109
etechpro <technov8ive@gmail.com>
2026-05-22 18:59:04 +0100

    add database creation logic to main.py

--- app/core/async_db.py
+++ app/core/async_db.py
@@ -1,8 +1,10 @@
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
 
 from app.core.config import settings
+
 
 # Load environment variable from `.env` file
 load_dotenv()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+import logging
+
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-
 from app.api.v1.payment_routes import v1_router as payment_router
-
+from app.core.async_db import async_engine, Base
+from app.models.payment import Payment
+from app.models.idempotency_record import IdempotencyRecord
 
 app = FastAPI(
     title="Payment Orchestration Service",
